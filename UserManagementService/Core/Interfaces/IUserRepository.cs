fcf74e11d46b40113b261b116eb53b7ef0e0017d
Nika Gurchiani <104095410+supernovaedge@users.noreply.github.com>
2024-12-14 18:54:34 +0100

    refactored repository interfaces

--- UserManagementService/Core/Interfaces/IUserRepository.cs
+++ UserManagementService/Core/Interfaces/IUserRepository.cs
@@ -2,13 +2,8 @@
 
 namespace UserManagementService.Core.Interfaces
 {
-    public interface IUserRepository
+    public interface IUserRepository : IRepository<User>
     {
-        Task<User?> GetByIdAsync(Guid id);
         Task<User?> GetByEmailAsync(string email);
-        Task<IEnumerable<User>> GetAllAsync();
-        Task AddAsync(User user);
-        Task UpdateAsync(User user);
-        Task DeleteAsync(Guid id);
     }
 }
