f27ff07c39bcdba6a9edf6020f9dbbdd69994856
Nika Gurchiani <104095410+supernovaedge@users.noreply.github.com>
2024-12-14 15:46:25 +0100

    Added Password and Email Specifications

---
+++ UserManagementService/Core/Specifications/PasswordStrengthSpecification.cs
@@ -0,0 +1,14 @@
+﻿namespace UserManagementService.Core.Specifications
+{
+    public class PasswordStrengthSpecification
+    {
+        public bool IsSatisfiedBy(string password)
+        {
+           
+            return password.Length >= 8 &&
+                   password.Any(char.IsUpper) &&
+                   password.Any(char.IsLower) &&
+                   password.Any(char.IsDigit);
+        }
+    }
+}
---
+++ UserManagementService/Core/Specifications/UniqueEmailSpecification.cs
@@ -0,0 +1,20 @@
+﻿using UserManagementService.Core.Interfaces;
+
+namespace UserManagementService.Core.Specifications
+{
+    public class UniqueEmailSpecification
+    {
+        private readonly IUserRepository _userRepository;
+
+        public UniqueEmailSpecification(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task<bool> IsSatisfiedByAsync(string email)
+        {
+            var user = await _userRepository.GetByEmailAsync(email);
+            return user == null;
+        }
+    }
+}
