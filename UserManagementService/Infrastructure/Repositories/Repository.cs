b38fab3b74a28cc8654896722d20f91b94b38fd5
Nika Gurchiani <104095410+supernovaedge@users.noreply.github.com>
2024-12-14 18:53:42 +0100

    refactored repository handling

---
+++ UserManagementService/Infrastructure/Repositories/Repository.cs
@@ -0,0 +1,54 @@
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+using UserManagementService.Core.Interfaces;
+using UserManagementService.Infrastructure.Data;
+
+namespace UserManagementService.Infrastructure.Repositories
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        protected readonly UserManagementDbContext _context;
+
+        public Repository(UserManagementDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<T?> GetByIdAsync(Guid id)
+        {
+            return await _context.Set<T>().FindAsync(id);
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await _context.Set<T>().ToListAsync();
+        }
+
+        public async Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate)
+        {
+            return await _context.Set<T>().Where(predicate).ToListAsync();
+        }
+
+        public async Task AddAsync(T entity)
+        {
+            await _context.Set<T>().AddAsync(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            _context.Set<T>().Update(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(Guid id)
+        {
+            var entity = await _context.Set<T>().FindAsync(id);
+            if (entity != null)
+            {
+                _context.Set<T>().Remove(entity);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
